631b21c3e098930d022ad96a5c7c990e99e20225
gifnksm <makoto.nksm+github@gmail.com>
2026-01-31 16:15:29 +0900

    fix: update web cache strategy

--- assets/sw.js
+++ assets/sw.js
@@ -1,10 +1,5 @@
-var cacheName = "numelace-pwa";
-var filesToCache = [
-    "./",
-    "./index.html",
-    "./numelace.js",
-    "./numelace_bg.wasm",
-];
+var cacheName = "numelace-pwa-v1";
+var filesToCache = ["./", "./index.html"];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener("install", function (e) {
@@ -15,11 +10,56 @@ self.addEventListener("install", function (e) {
     );
 });
 
+/* Remove old caches on activate */
+self.addEventListener("activate", function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keys) {
+            return Promise.all(
+                keys.map(function (key) {
+                    if (key !== cacheName) {
+                        return caches.delete(key);
+                    }
+                }),
+            );
+        }),
+    );
+});
+
 /* Serve cached content when offline */
 self.addEventListener("fetch", function (e) {
+    var requestUrl = new URL(e.request.url);
+
+    if (
+        requestUrl.pathname.endsWith("/index.html") ||
+        e.request.mode === "navigate"
+    ) {
+        e.respondWith(
+            fetch(e.request)
+                .then(function (response) {
+                    return caches.open(cacheName).then(function (cache) {
+                        cache.put(e.request, response.clone());
+                        return response;
+                    });
+                })
+                .catch(function () {
+                    return caches.match(e.request);
+                }),
+        );
+        return;
+    }
+
     e.respondWith(
         caches.match(e.request).then(function (response) {
-            return response || fetch(e.request);
+            if (response) {
+                return response;
+            }
+
+            return fetch(e.request).then(function (networkResponse) {
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, networkResponse.clone());
+                    return networkResponse;
+                });
+            });
         }),
     );
 });
